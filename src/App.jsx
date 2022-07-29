260ec44cd19ddaad8d4e7628f6f7e0f5cfce88b6
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-07-30 01:42:39 +0530

    Added Styles

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,16 @@ import {
   Grid,
   Toolbar,
   Container,
+  Button,
 } from '@mui/material';
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
+
+import {
+  createTheme,
+  styled,
+  ThemeProvider,
+  darken,
+} from '@mui/material/styles';
 
 const App = () => {
   return (
@@ -26,6 +34,43 @@ const App = () => {
           </Typography>
         </Toolbar>
       </AppBar>
+      <main>
+        <div>
+          <Container maxWidth="sm" style={{ marginTop: '100px' }}>
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Molestiae assumenda consectetur voluptate pariatur nisi eum!
+            </Typography>
+            <div>
+              <Grid container spacing={2} justifyContent="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    See My Photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary Actions
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+      </main>
     </>
   );
 };
